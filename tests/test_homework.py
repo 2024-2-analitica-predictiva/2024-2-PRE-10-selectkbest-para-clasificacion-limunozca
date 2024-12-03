7263c210aae2e85454ca5a44c83b8c29a0d7dae9
limunozca <limunozca@unal.edu.co>
2024-12-02 20:57:22 -0500

    Taller10

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,11 +5,11 @@ def load_data():
 
     import pandas as pd
 
-    dataset = pd.read_csv("heart_disease.csv")
+    dataset = pd.read_csv("files/input/heart_disease.csv")
     y = dataset.pop("target")
     x = dataset.copy()
     x["thal"] = x["thal"].map(
-        lambda x: "normal" if x not in ["fixed", "fixed", "reversible"] else x
+        lambda x: "normal" if x not in ["fixed", "reversible"] else x
     )
 
     return x, y
@@ -20,9 +20,9 @@ def load_estimator():
     import os
     import pickle
 
-    if not os.path.exists("estimator.pickle"):
+    if not os.path.exists("homework/estimator.pickle"):
         return None
-    with open("estimator.pickle", "rb") as file:
+    with open("homework/estimator.pickle", "rb") as file:
         estimator = pickle.load(file)
 
     return estimator
